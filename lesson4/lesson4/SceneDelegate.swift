c0ddb9c3d4dd27f935cf2c776ab15570697b2b71
valerieguch <valeria.guch2000@gmail.com>
2022-01-24 10:04:08 +0300

    created project

--- lesson4/lesson4/SceneDelegate.swift
+++ lesson4/lesson4/SceneDelegate.swift
@@ -1,9 +1,8 @@
 //
 //  SceneDelegate.swift
-//  Lesson1
+//  lesson4
 //
-//  Created by Pantheon Sharen on 10/15/21.
-//  Copyright © 2021 Pantheon Sharen. All rights reserved.
+//  Created by Валерия Гучустян on 1/24/22.
 //
 
 import UIKit
@@ -24,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -46,6 +45,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
